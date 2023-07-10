7450ac90bc3777237aae19b529f8a1ad8fef33d3
Bib711 <123845805+Bib711@users.noreply.github.com>
2023-07-10 10:15:14 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -1,9 +1,6 @@
 // import java.util.Scanner;
-<<<<<<< HEAD
 // Simple java calcu  pgm by bib
-=======
 // Updated by Bibin
->>>>>>> main
 import java.util.*;
 public class loops {
     public static void main(String[] args) {
